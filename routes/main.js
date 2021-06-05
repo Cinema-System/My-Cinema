cd40c85f81a2ddad6771d4aa102fcc323978df36
jino <bubble3jh@naver.com>
2021-06-05 23:08:02 +0900

    main 끝

--- routes/main.js
+++ routes/main.js
@@ -9,4 +9,20 @@ router.get("/signup", function (req, res) {
   res.render("main/signup", { title: "signUp" });
 });
 
+router.get("/verification", function (req, res) {
+  res.render("main/verification", { title: "signUp" });
+});
+
+router.get("/complete", function (req, res) {
+  res.render("main/verification_complete", { title: "signUp" });
+});
+
+router.get("/nonsignup", function (req, res) {
+  res.render("main/nonsignup", { title: "signUp" });
+});
+
+router.get("/movieshow", function (req, res) {
+  res.render("main/movieshow", { title: "signUp" });
+});
+
 module.exports = router;
