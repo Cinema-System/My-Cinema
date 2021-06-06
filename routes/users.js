333d1695f604989742cb15d663533ade78f15c0c
jino <bubble3jh@naver.com>
2021-06-06 21:15:02 +0900

    what?

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,6 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
-module.exports = router;
-var express = require("express");
-var router = express.Router();
-
 /* GET home page. */
 //라우터의 get()함수를 이용해 request URL('/')에 대한 업무처리 로직 정의
 router.get("/", function (req, res) {
