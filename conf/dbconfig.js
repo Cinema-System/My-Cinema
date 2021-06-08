cc4229124907fcd24a27c6e91aea913df6359453
DaaEun <didekdms123@gmail.com>
2021-06-08 12:29:47 +0900

    백엔드 연결

--- conf/dbconfig.js
+++ conf/dbconfig.js
@@ -1,26 +1,12 @@
 /*  DB Info */
 
-// module.exports = {
-//     hrPool: {
-//       user: "jinho",//process.env.HR_USER,
-//       password: "1227",//process.env.HR_PASSWORD,
-//       connectString: "localhost/XE",//process.env.HR_CONNECTIONSTRING,
-//       // 아래 pool관련 속성들에 대해선 아직 저도 잘 모릅니다!
-//       poolMin: 10,
-//       poolMax: 10,
-//       poolIncrement: 0,
-//       multipleStatements: true,
-//     }
-//    };
-
 module.exports = 
 {
-    user : process.env.NODE_ORACLEDB_USER || "jinho",
-    password : process.env.NODE_ORACLEDB_PASSWOR || "1227",
-    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/XE",
-    externalAuth : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
-};
-
+    user : process.env.NODE_ORACLEDB_USER || "cinema",
+    password : process.env.NODE_ORACLEDB_PASSWOR || "123456",
+    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/xe"
+    // externalAuth : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
+}
 
 // module.exports = {
 //     user : process.env.NODE_ORACLEDB_USER || "계정아이디",
@@ -39,3 +25,18 @@ module.exports =
 //     // https://github.com/oracle/node-oracledb/blob/master/doc/api.md#extauth
 //     externalAuth : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
 //     };
+
+// -----------------------------------------------------------------------------------
+
+// module.exports = {
+//     hrPool: {
+//       user: "jinho",//process.env.HR_USER,
+//       password: "1227",//process.env.HR_PASSWORD,
+//       connectString: "localhost/XE",//process.env.HR_CONNECTIONSTRING,
+//       // 아래 pool관련 속성들에 대해선 아직 저도 잘 모릅니다!
+//       poolMin: 10,
+//       poolMax: 10,
+//       poolIncrement: 0,
+//       multipleStatements: true,
+//     }
+//    };
--- routes/db.js
+++ routes/db.js
@@ -51,57 +51,66 @@ router.post("/dbTestSelect", function (request, response) {
 });
 
 // 데이터 입력 처리
-router.post("/dbTestInsert", function (request, response) {
-  oracledb.getConnection(
-    {
-      user: dbConfig.user,
-      password: dbConfig.password,
-      connectString: dbConfig.connectString,
-    },
-    function (err, connection) {
+router.post('/dbTestInsert', function(request, response){
+
+  oracledb.getConnection({
+      user            : dbConfig.user,
+      password        : dbConfig.password,
+      connectString   : dbConfig.connectString
+  },
+  function(err, connection) {
       if (err) {
-        console.error(err.message);
-        return;
+          console.error(err.message);
+          return;
       }
 
+      // // PrepareStatement 구조
+      // let query =
+      //   "INSERT INTO EMP( EMPNO ,ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO) " +
+      //   "VALUES( :EMPNO ,:ENAME, :JOB, :MGR, SYSDATE, :SAL, :COMM, :DEPTNO )";
+
+      // let binddata = [
+      //   Number(request.body.empno),
+      //   request.body.ename,
+      //   request.body.job,
+      //   request.body.mgr,
+      //   Number(request.body.sal),
+      //   Number(request.body.comm),
+      //   Number(request.body.deptno),
+      // ];
+
       // PrepareStatement 구조
-      let query =
-        "INSERT INTO EMP( EMPNO ,ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO) " +
-        "VALUES( :EMPNO ,:ENAME, :JOB, :MGR, SYSDATE, :SAL, :COMM, :DEPTNO )";
+      let query = 
+          'INSERT INTO LOCAL(LOCALNO, LOCALNAME) ' +
+            'VALUES( :LOCALNO, :LOCALNAME)';
 
       let binddata = [
-        Number(request.body.empno),
-        request.body.ename,
-        request.body.job,
-        request.body.mgr,
-        Number(request.body.sal),
-        Number(request.body.comm),
-        Number(request.body.deptno),
+          'LOCAL08',
+          '다은마을'        
       ];
 
       connection.execute(query, binddata, function (err, result) {
-        if (err) {
-          console.error(err.message);
-          doRelease(connection);
-          return;
-        }
-        console.log("Row Insert: " + result.rowsAffected);
+          if (err) {
+              console.error(err.message);
+              doRelease(connection);
+              return;
+          }
+          console.log('Row Insert: ' + result.rowsAffected);
 
-        doRelease(connection, result.rowsAffected); // Connection 해제
+          doRelease(connection, result.rowsAffected);         // Connection 해제
       });
-    }
-  );
+  });    
 
   // DB 연결 해제
   function doRelease(connection, result) {
-    connection.release(function (err) {
-      if (err) {
-        console.error(err.message);
-      }
+      connection.release(function (err) {
+          if (err) {
+              console.error(err.message);
+          }
 
-      // DB종료까지 모두 완료되었을 시 응답 데이터 반환
-      response.send("" + result);
-    });
+          // DB종료까지 모두 완료되었을 시 응답 데이터 반환
+          response.send(''+result);
+      });
   }
 });
 
