1764465450fb486d10fa3fed1f568acec5e90578
jino <bubble3jh@gmail.com>
2021-06-08 13:58:16 +0900

    maybe?

--- conf/dbconfig.js
+++ conf/dbconfig.js
@@ -1,26 +1,24 @@
 /*  DB Info */
 
-module.exports = 
-{
-    user : process.env.NODE_ORACLEDB_USER || "cinema",
-    password : process.env.NODE_ORACLEDB_PASSWOR || "123456",
-    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/xe"
-    // externalAuth : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
-}
+module.exports = {
+  user: process.env.NODE_ORACLEDB_USER || "c##cinema",
+  password: process.env.NODE_ORACLEDB_PASSWOR || "123456",
+  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/xe",
+  // externalAuth : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
+};
 
 // module.exports = {
 //     user : process.env.NODE_ORACLEDB_USER || "계정아이디",
-    
+
 //     // Instead of hard coding the password, consider prompting for it,
 //     // passing it in an environment variable via process.env, or using
 //     // External Authentication.
 //     password : process.env.NODE_ORACLEDB_PASSWORD || "비밀번호",
-    
+
 //     // For information on connection strings see:
 //     // https://github.com/oracle/node-oracledb/blob/master/doc/api.md#connectionstrings
 //     connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "호스트이름/자신의 sid",
-    
-    
+
 //     // Setting externalAuth is optional. It defaults to false. See:
 //     // https://github.com/oracle/node-oracledb/blob/master/doc/api.md#extauth
 //     externalAuth : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
--- routes/manager.js
+++ routes/manager.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
-
+var oracledb = require("oracledb");
+var dbConfig = require("../conf/dbconfig");
 /* GET home page. */
 //라우터의 get()함수를 이용해 request URL('/')에 대한 업무처리 로직 정의
 router.get("/goerway", function (req, res) {
@@ -27,6 +28,43 @@ router.get("/schedule", function (req, res) {
 });
 router.get("/memchoice", function (req, res) {
   res.render("manager/memchoice", { title: "Express" });
+});
+
+router.post("/local", function (request, response) {
+  const itemName = request.body.newItem;
+
+  oracledb.getConnection(
+    {
+      user: dbConfig.user,
+      password: dbConfig.password,
+      connectString: dbConfig.connectString,
+    },
+    function (err, connection) {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+
+      let query =
+        "INSERT INTO LOCAL(LOCALNO, LOCALNAME) " +
+        "VALUES( :LOCALNO, :LOCALNAME)";
+
+      let binddata = ["LOCAL08", itemName];
+
+      connection.execute(query, binddata, function (err, result) {
+        if (err) {
+          console.error(err.message);
+          doRelease(connection);
+          return;
+        }
+        console.log("Row Insert: " + result.rowsAffected);
+
+        doRelease(connection, result.rowsAffected); // Connection 해제
+      });
+    }
+  );
+  response.render("manager/local");
+  console.log(itemName);
 });
 
 //모듈에 등록해야 app.js에서 app.use 함수를 통해서 사용 가능
