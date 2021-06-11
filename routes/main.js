6fd49fe7b9aecd18ea6e7d208068b6bf3e31e3b1
jino <bubble3jh@gmail.com>
2021-06-11 15:11:09 +0900

    sql checkout

--- routes/main.js
+++ routes/main.js
@@ -6,9 +6,49 @@ router.get("/login", function (req, res) {
 });
 
 router.get("/signup", function (req, res) {
-  res.render("main/signup", { title: "signUp" });
+  res.render("main/signup", { username: "signUp" });
 });
 
+router.post("/", async function (req, res) {
+  // create: 회원 생성
+    oracledb.getConnection(
+    {
+      user: dbConfig.user,
+      password: dbConfig.password,
+      connectString: dbConfig.connectString,
+    },
+    function (err, connection) {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+
+      let query =
+        "INSERT INTO LOCAL(LOCALNO, LOCALNAME) " +req.body.userid
+        "VALUES( :LOCALNO, :LOCALNAME)";
+
+      let binddata = ["LOCAL08", "다은마을"];
+
+      connection.execute(query, binddata, function (err, result) {
+        if (err) {
+          console.error(err.message);
+          doRelease(connection);
+          return;
+        }
+        console.log("Row Insert: " + result.rowsAffected);
+
+        doRelease(connection, result.rowsAffected); // Connection 해제
+      });
+    }
+  );
+
+  // DB 연결 해제
+  function doRelease(connection, result) {
+    connection.release(function (err) {
+      if (err) {
+        console.error(err.message);
+      }
+});
 router.get("/verification", function (req, res) {
   res.render("main/verification", { title: "signUp" });
 });
--- routes/manager.js
+++ routes/manager.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var oracledb = require("oracledb");
 var dbConfig = require("../conf/dbconfig");
+
 /* GET home page. */
 //라우터의 get()함수를 이용해 request URL('/')에 대한 업무처리 로직 정의
 router.get("/goerway", function (req, res) {
@@ -29,37 +30,111 @@ router.get("/schedule", function (req, res) {
 router.get("/memchoice", function (req, res) {
   res.render("manager/memchoice", { title: "Express" });
 });
+// function getlocalcnt() {
+//   oracledb.getConnection(
+//     {
+//       user: dbConfig.user,
+//       password: dbConfig.password,
+//       connectString: dbConfig.connectString,
+//     },
+//     function (err, connection) {
+//       if (err) {
+//         console.error(err.message);
+//         return;
+//       }
 
+//       let query = "SELECT LOCALNO " + "   FROM LOCAL";
+
+//       connection.execute(query, [], function (err, result) {
+//         if (err) {
+//           console.error(err.message);
+//           // doRelease(connection);
+//           return;
+//         }
+//         for (i = 0; i < result.rows.length; i++) {
+//           resultint = result.rows[i][0].slice(-2) * 1;
+//           if (localmax <= resultint) {
+//             localmax = resultint;
+//           }
+//         }
+//         console.log(localmax + "in");
+//         // doRelease(connection, result.rows); // Connection 해제
+
+//         console.log(localmax + "out");
+//         localmax += "";
+//         localmax = String("0" + localmax).slice(-2);
+//         localcnt = localmax;
+//         console.log(localmax + "out2");
+//         console.log(localcnt + "out2");
+//       });
+//     }
+//   );
+// }
+
+//주요한 문제 : insert execute를 먼저 실행하고 select를 실행하여 무결성 제약 조건 위반한대 자꾸 진짜 ㅡㅡ
 router.post("/local", function (request, response) {
   const itemName = request.body.newItem;
 
+  var localcnt = "00";
+  var localmax = 0;
+  var resultint = "";
+
   oracledb.getConnection(
+    //접근정보 가져옴
     {
       user: dbConfig.user,
       password: dbConfig.password,
       connectString: dbConfig.connectString,
     },
+
     function (err, connection) {
       if (err) {
         console.error(err.message);
         return;
       }
+      //지역번호 가져옴
+      let selquery = "SELECT LOCALNO " + "   FROM LOCAL";
 
-      let query =
-        "INSERT INTO LOCAL(LOCALNO, LOCALNAME) " +
-        "VALUES( :LOCALNO, :LOCALNAME)";
-
-      let binddata = ["LOCAL08", itemName];
-
-      connection.execute(query, binddata, function (err, result) {
+      connection.execute(selquery, [], function (err, result) {
         if (err) {
           console.error(err.message);
-          doRelease(connection);
+          // doRelease(connection);
+          return;
+        }
+        // 컬럼 수만큼 돌면서 마지막 2자리 문자열을 숫자화 후 비교하여 최댓값을 localmax에 대입
+        for (i = 0; i < result.rows.length; i++) {
+          resultint = result.rows[i][0].slice(-2) * 1;
+          if (localmax <= resultint) {
+            localmax = resultint;
+          }
+        }
+        // doRelease(connection, result.rows); // Connection 해제 <<오류남>>
+
+        //0x xx등 2자리 수로 만들어준 다음 문자열화 시켜서 localcnt에 삽입
+        localmax += "";
+        localmax = String("0" + localmax).slice(-2);
+        localcnt = localmax;
+      });
+      //localcnt 에서 숫자를 1 늘리고 문자열화 시켜서 삽임
+      localcnt++;
+      localcnt += "";
+      localcnt = String("0" + localcnt).slice(-2);
+      //삽입쿼리
+      let insertquery =
+        "INSERT INTO LOCAL(LOCALNO, LOCALNAME) " +
+        "VALUES( :LOCALNO, :LOCALNAME)";
+      //LOCAL 뒤에 2자리 숫자 붙혀서 item과 함께 삽입
+      let binddata = ["LOCAL" + localcnt, itemName];
+      // inclocalcnt();
+      //insert
+      connection.execute(insertquery, binddata, function (err, result) {
+        if (err) {
+          console.error(err.message);
+          // doRelease(connection);
           return;
         }
         console.log("Row Insert: " + result.rowsAffected);
-
-        doRelease(connection, result.rowsAffected); // Connection 해제
+        // doRelease(connection, result.rowsAffected); // Connection 해제
       });
     }
   );
